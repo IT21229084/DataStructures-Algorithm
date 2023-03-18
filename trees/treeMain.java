ff80080ae6f490a841a95569c3c4881a3a6409d0
IT21229084 <99582850+IT21229084@users.noreply.github.com>
2023-03-18 17:34:13 +0530

    Update treeMain.java
    add new methods

--- trees/treeMain.java
+++ trees/treeMain.java
@@ -30,5 +30,28 @@ public class treeMain {
 
 		
 	}
+	private Node findRecursive(Node localRoot, int empno) {
+    	 
+    	 if(localRoot == null) {
+    		 return null;
+  	 }
+  	     else if(localRoot.empNo ==empno) {
+  		 System.out.println(localRoot.empName);
+          		 return localRoot;
+    	 }
+  	     else if (empno <localRoot.empNo) {
+  	    	 return findRecursive(localRoot.leftChild,empno);
+  	     }else {
+  	    	return findRecursive(localRoot.rightChild,empno);
+  	    	 
+  	     }
+    	 
+     }
+	 public void deleteAll() {
+    	 root = null;
+     }
+	 public void traversepreorder() {
+		 preOrder(root);
+	}
 
 }
