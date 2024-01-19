9c8cc82e079162df871c20bc7b66b933d2fab35a
Malith Iroshan <99582850+IT21229084@users.noreply.github.com>
2024-01-19 14:11:10 +0530

    Add BubbleSort

---
+++ BubbleSort.py
@@ -0,0 +1,18 @@
+ A = []
+  for v in range(1,5):
+     A.append(int(input("Enter Values: ")))
+  
+  print(A)
+   
+
+
+ def BubbleSort(A)
+   n = len(A)
+   
+   for i in Range(1,n):
+     for J in Range(1,n-i+1):
+       if(A[j] < A[j-1]:
+         A[j],A[j-1] = A[j-1],A[j]
+
+BubbleSort(A)
+print(A)
